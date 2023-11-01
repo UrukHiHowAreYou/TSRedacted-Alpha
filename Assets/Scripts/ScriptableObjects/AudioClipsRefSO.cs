788adf08cacb683e86d292245ccbd806489335d7
The Human Abrahams <thehumanabrahams@gmail.com>
2023-11-01 00:04:48 +0000

    adding missing scripts for Audio clips SO

---
+++ Assets/Scripts/ScriptableObjects/AudioClipsRefSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.AssetImporters;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class AudioClipsRefSO : ScriptableObject
+{
+    public AudioClip[] threeTwoOne;
+    //public AudioClip two;
+    //public AudioClip one;
+
+}
