1cbaa18f7b148f6aa9b1f23a6bad57681cd6db4c
ReachRyanUK <reachryanuk@gmail.com>
2023-11-09 23:11:20 +0000

    UNDID THE BAD: Reverted the baking
    Also added lightswitch to Main menu

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -8,21 +8,30 @@ public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] private Button playButton;
     [SerializeField] private Button quitButton;
+    [SerializeField] private Button lightSwitch;
+    [SerializeField] private Light ceilingLight;
 
     private void Start()
     {
         playButton.onClick.AddListener(() =>
         {
-            Debug.Log("Hit Play!");
+            Debug.Log("Play it again Sam!");
             Loader.Load(Loader.Scene.Playground);
         }
         );
         quitButton.onClick.AddListener(() =>
         {
-            Debug.Log("Hit Quit :( ");
+            Debug.Log("Hit (it and) Quit (it) :() ");
             Application.Quit();
         }
         );
+        lightSwitch.onClick.AddListener(() =>
+        {
+            Debug.Log("Hit the Splights!");
+            ceilingLight.enabled = !ceilingLight.enabled;
+        }
+        );
+
 
         Time.timeScale = 1f;
     }
