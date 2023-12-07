6e920f45494f306ad999610b07ff7f168904ff3b
ReachRyanUK <reachryanuk@gmail.com>
2023-12-07 17:48:22 +0000

    Added: Logger, NetworkSingleton, Singleton core scripts
    Added some core scripts and components. Updated NetworkManager to utilise logger for adding debug info and created PlayerManager to track number of players.

--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class NetworkManagerUI : MonoBehaviour
 {
@@ -10,16 +11,24 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField] private Button serverBtn;
     [SerializeField] private Button hostBtn;
     [SerializeField] private Button clientBtn;
+    [SerializeField] private TextMeshProUGUI playersInGameText;
     
     private void Awake() {
         serverBtn.onClick.AddListener(() => {
             NetworkManager.Singleton.StartServer();
+            Logger.Instance.LogInfo("Server started...");
         });
         hostBtn.onClick.AddListener(() => {
             NetworkManager.Singleton.StartHost();
+            Logger.Instance.LogInfo("Host started...");
         });
         clientBtn.onClick.AddListener(() => {
             NetworkManager.Singleton.StartClient();
+            Logger.Instance.LogInfo("Client started...");
         });
+    }
+
+    private void Update() {
+        playersInGameText.text = $"Players in game: {PlayersManager.Instance.PlayersInGame}";
     }
 }
